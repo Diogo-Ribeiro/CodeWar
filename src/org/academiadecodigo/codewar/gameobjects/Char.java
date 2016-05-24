c2b4123e3e7268998f78c50908a543ad78942fb1
Diogo Ribeiro <xaymyte@gmail.com>
2016-05-24 10:55:02 +0100

    Add Char properties and Codecadet properties

--- src/org/academiadecodigo/codewar/gameobjects/Char.java
+++ src/org/academiadecodigo/codewar/gameobjects/Char.java
@@ -4,4 +4,21 @@ package org.academiadecodigo.codewar.gameobjects;
  * Created by diogocodecadet on 23/05/16.
  */
 public abstract class Char extends GameObjects {
+
+    private int hp;
+    private boolean dead;
+    private boolean bug;
+
+
+
+    public static void move (){
+
+    }
+    public void shoot(){
+
+    }
+
+    public void dead () {
+
+    }
 }
--- src/org/academiadecodigo/codewar/gameobjects/Codecadet.java
+++ src/org/academiadecodigo/codewar/gameobjects/Codecadet.java
@@ -4,4 +4,28 @@ package org.academiadecodigo.codewar.gameobjects;
  * Created by diogocodecadet on 23/05/16.
  */
 public class Codecadet extends Char {
+
+    private int hp;
+    private boolean dead;
+    private boolean bug;
+
+
+    public Codecadet(){
+
+    }
+
+    public static void move (){
+
+    }
+    public void shoot(){
+
+    }
+
+    public void dead () {
+
+    }
+
+
+
+
 }
