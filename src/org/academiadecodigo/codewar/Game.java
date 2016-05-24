d32da04d1b75ba6602d73f5078045c8c7f81c8c9
Laura Nunes <lauranfs@hotmail.com>
2016-05-24 10:53:37 +0100

    Added Catarina's TODOs

--- src/org/academiadecodigo/codewar/Game.java
+++ src/org/academiadecodigo/codewar/Game.java
@@ -26,5 +26,5 @@ public class Game {
 
     public void start () {}
 
-    
+
 }
--- src/org/academiadecodigo/codewar/Main.java
+++ src/org/academiadecodigo/codewar/Main.java
@@ -4,4 +4,5 @@ package org.academiadecodigo.codewar;
  * Created by diogocodecadet on 23/05/16.
  */
 public class Main {
+    // TODO: 24/05/16 change name to beijinhos & caralhinhos
 }
