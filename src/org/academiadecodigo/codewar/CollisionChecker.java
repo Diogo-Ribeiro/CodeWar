d871d8cd8bee54bbde258c3c724bb8f9aff0cfe4
Laura Nunes <lauranfs@hotmail.com>
2016-05-25 14:39:15 +0100

    Added Collision Checker

--- src/org/academiadecodigo/codewar/CollisionChecker.java
+++ src/org/academiadecodigo/codewar/CollisionChecker.java
@@ -1,7 +1,26 @@
 package org.academiadecodigo.codewar;
 
+import org.academiadecodigo.codewar.gameobjects.Char;
+import org.academiadecodigo.codewar.gameobjects.Projectile;
+
 /**
  * Created by codecadet on 25/05/16.
  */
 public class CollisionChecker {
+
+    public static void check(Projectile[] projectiles, Char[] chars) {
+
+        for (int i = 0; i < chars.length; i++) {
+
+            for (int j = 0; j < chars.length; j++) {
+
+                if (projectiles[i].getPos() == chars[i].getPos()) {
+
+                    projectiles[i].hitTarget(); //update boolean, says it can be deleted
+                    chars[i].getHit (projectiles[i]);
+
+                }
+            }
+        }
+    }
 }
