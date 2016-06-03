1453f6f9c4ea022a937140e61c33d45b0187dcb2
Amauri Nakashima <amauri.nakashima@gmail.com>
2016-06-03 16:35:34 +0100

    Documentation of Char and moved method die() to codeCadet and field heath

--- src/org/academiadecodigo/codewar/gameobjects/Char.java
+++ src/org/academiadecodigo/codewar/gameobjects/Char.java
@@ -4,35 +4,41 @@ import org.academiadecodigo.codewar.representable.GridPosition;
 
 /**
  * Created by diogocodecadet on 23/05/16.
+ * Represents all possible characters of the game.
  */
 public abstract class Char extends GameObjects {
 
+    /**
+     * @param AVATAR
+     */
     public final static int AVATAR_DIMENSION = 4;
-    private int hp = 6;
     private boolean dead;
     private boolean moving;
 
+    /**
+     * Constructor of class Char.
+     * @param position
+     */
     public Char (GridPosition position) {
 
         super (position);
     }
 
+    /**
+     * makes the character move.
+     * call position.move().
+     */
     public abstract void move ( );
 
+    /**
+     * @return Projectile.
+     */
     public abstract Projectile shoot();
 
-    //Amauri: Teste para o checkCollision
-    //public abstract void getHit(Projectile projectile);
-
-    public void lowerHP() {
-
-        hp--;
-
-        if (hp == 0) {
-
-            dead = true;
-        }
-    }
+    /**
+     * remove character.
+     * set field dead to true.
+     */
 
     public void die () {
 
@@ -40,14 +46,26 @@ public abstract class Char extends GameObjects {
         dead = true;
     }
 
+    /**
+     * check if character is dead.
+     * @return true if it is dead.
+     */
     public boolean isDead() {
         return dead;
     }
 
+    /**
+     * check if character is moving.
+     * @return true if it is moving.
+     */
     public boolean isMoving() {
         return moving;
     }
 
+    /**
+     * set field moving according to argument.
+     * @param moving
+     */
     public void setMoving(boolean moving) {
         this.moving = moving;
     }
