309ac95d97fbc778ad2f7208f870a5c088720227
Diogo Ribeiro <xaymyte@gmail.com>
2016-06-03 17:45:50 +0100

    Menu commented.

--- src/org/academiadecodigo/codewar/Menu.java
+++ src/org/academiadecodigo/codewar/Menu.java
@@ -6,7 +6,6 @@ import org.academiadecodigo.codewar.representable.Grid;
 import org.academiadecodigo.codewar.representable.SimpleGfxGrid;
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
-import org.academiadecodigo.simplegraphics.graphics.Text;
 import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
@@ -18,7 +17,7 @@ import static org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent.*;
 /**
  * Created by codecadet on 01/06/16.
  */
-public class Menu implements KeyboardHandler{
+public class Menu implements KeyboardHandler {
 
     private Keyboard k;
     private Rectangle rectangle;
@@ -27,29 +26,49 @@ public class Menu implements KeyboardHandler{
     private Picture pic;
     private Grid grid;
 
+    /**
+     * Menu Constructor that recieves a grid.
+     *
+     * @param grid
+     */
     public Menu(Grid grid) {
         this.grid = grid;
     }
+
+    /**
+     * Init method that initiate the game. Creates a grid with a picture "menu.jpg" and creates a grid.
+     * The KeyBoardInput method is called here to be possible to select the codecadet.
+     */
 
     public void init() {
 
         pic = new Picture();
         pic.load("resources/menu.jpg");
-        pic.grow((grid.getCols()*grid.getCellSize() - pic.getWidth())/2, (grid.getRows()*grid.getCellSize()- pic.getHeight())/2);
+        pic.grow((grid.getCols() * grid.getCellSize() - pic.getWidth()) / 2, (grid.getRows() * grid.getCellSize() - pic.getHeight()) / 2);
         pic.translate(-pic.getX(), -pic.getY());
         pic.draw();
 
 
-        rectangle = new Rectangle(0,0,Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE, Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE);
+        rectangle = new Rectangle(0, 0, Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE, Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE);
         rectangle.setColor(Color.RED);
         rectangle.draw();
         registerKeyboardInput();
     }
 
+    /**
+     * The choose() method creates a grid of codecadets and for each one draws his picture.
+     * Until the user press the Keyboard_K, the choose method stays in the while loop.
+     * After the player make them choose, the choose method deletes all the codecadet's pictures and call method init() of the game grid.
+     *
+     * @return CodeCadetType
+     * @throws InterruptedException
+     */
+
+
     public CodecadetType choose() throws InterruptedException {
 
         Picture[] cadets = new Picture[CodecadetType.values().length];
-        for (int i = 0; i < cadets.length; i++ ) {
+        for (int i = 0; i < cadets.length; i++) {
 
             cadets[i] = CodecadetType.values()[i].getRepresentable();
 
@@ -65,7 +84,7 @@ public class Menu implements KeyboardHandler{
 
             if (i > 9) {
 
-                cadets[i].translate(cadets[i-1].getMaxX(), cadets[i].getHeight());
+                cadets[i].translate(cadets[i - 1].getMaxX(), cadets[i].getHeight());
             }
 
             cadets[i].draw();
@@ -88,6 +107,15 @@ public class Menu implements KeyboardHandler{
         return CodecadetType.values()[i];
     }
 
+    /**
+     * The KeyPressed method recieves a keyboardEvent from the keyboard, and translate that Key input in a game action.
+     * KEY_UP - Iterate and move the selection rectangle to Backwards.
+     * KEY_DOWN - Iterate and move the selection rectangle to Forwards.
+     * KEY_C - Select the codecadet and set the propriety notDone to false.
+     *
+     * @param keyboardEvent
+     */
+
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
 
@@ -95,10 +123,10 @@ public class Menu implements KeyboardHandler{
 
             case KEY_UP:
                 if (i == 9) {
-                    rectangle.translate(Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE*8, -Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE);
+                    rectangle.translate(Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE * 8, -Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE);
                     i--;
                 } else if (i <= 17 && i > 0) {
-                    rectangle.translate(-Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE, 0);
+                    rectangle.translate(-Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE, 0);
                     i--;
                 }
                 break;
@@ -106,11 +134,11 @@ public class Menu implements KeyboardHandler{
             case KEY_DOWN:
 
                 if (i == 8) {
-                    rectangle.translate(-Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE*8, Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE);
-                    i ++;
+                    rectangle.translate(-Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE * 8, Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE);
+                    i++;
                 } else if (i >= 0 && i < 17) {
-                    rectangle.translate(Char.AVATAR_DIMENSION* SimpleGfxGrid.CELL_SIZE, 0);
-                    i ++;
+                    rectangle.translate(Char.AVATAR_DIMENSION * SimpleGfxGrid.CELL_SIZE, 0);
+                    i++;
                 }
                 break;
 
@@ -120,11 +148,19 @@ public class Menu implements KeyboardHandler{
         }
     }
 
+    /**
+     * Does nothing at all.
+     *
+     * @param keyboardEvent
+     */
     @Override
     public void keyReleased(KeyboardEvent keyboardEvent) {
 
     }
 
+    /**
+     * The registerKeyboardInput register the keyboard keys and add the keyboardEvent to the EventListener.
+     */
     private void registerKeyboardInput() {
 
         k = new Keyboard(this);
