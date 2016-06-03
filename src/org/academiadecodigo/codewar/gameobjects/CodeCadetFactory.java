1ecb2ec025e4e85d71ad16d4b94a372a222a961a
Henry Robalo <henryrobalo@sapo.pt>
2016-06-03 18:28:15 +0100

    add comments

--- src/org/academiadecodigo/codewar/gameobjects/CodeCadetFactory.java
+++ src/org/academiadecodigo/codewar/gameobjects/CodeCadetFactory.java
@@ -3,10 +3,16 @@ package org.academiadecodigo.codewar.gameobjects;
 import org.academiadecodigo.codewar.representable.Grid;
 
 /**
- * Created by codecadet on 30/05/16.
+ * Create Code Cadet.
  */
 public class CodeCadetFactory {
 
+
+    /**
+     * @param grid
+     * @param type
+     * @return Code Cadet in the middle of the grid.
+     */
     public static Codecadet make (Grid grid, CodecadetType type) {
 
         return new Codecadet(grid.makeGridPosition(grid.getCols() / 2, grid.getRows() - (Char.AVATAR_DIMENSION), type.getRepresentable()));
